35782c35172f8720337b020de1fc5fe07de7da33
naveen kumar <84325007+navvenkumar@users.noreply.github.com>
2021-05-26 10:55:44 +0530

    Delete class.py

--- class.py
+++
@@ -1,6 +0,0 @@
-#1.Write a python program to remove the nth index character from a nonempty string
-str1 = "program"
-index = input('Enter the index value:')
-index = int(index)
-str = str1[0:index]+str1[index+1:]
-print(str)

d5b1709adc27dbe53d40c70ea7da706f086dc789
naveen kumar <84325007+navvenkumar@users.noreply.github.com>
2021-05-26 10:56:49 +0530

    Delete program6.py

--- program6.py
+++
@@ -1,7 +0,0 @@
-#6. Write a python program to find the repeated items of a tuple.
-
-tup = [2,3,4,5,3,4,2]
-alist = list(tup)
-from collections import Counter
-result = dict(Counter(alist))
-print(result)
